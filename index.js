b904316792b0a88225162067f32a262146309f5b
paige <88659700+paishee@users.noreply.github.com>
2024-02-27 12:51:11 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+function chore( strings, ...values) {
+    let [ name ] = (strings instanceof Array) ? strings : [strings];
+    let stuff = name;
+
+    function format(str) {
+        return str.replace(":", "").replace("=", "").replace(/ +(?= )/g, "").trim()
+    }
+
+    if (name == '') {
+        name = values.shift();
+        stuff = format(arguments[0][1]);
+    }
+
+
+    let value;
+    if (values.length == 0) value = eval(
+        format(((stuff.includes(":")) ? stuff.split(":") : stuff.split("=")).pop())
+    );
+    else value = values[0];
+
+
+    name = name.split(" ")[0];
+    name = format(name);
+
+
+    return (new Function(`value`, `return ${name} = value`))(value);
+}
+
+
+module.exports = chore;
