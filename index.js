344ef50eb209fed0a2ef9886fd6bee0ba684d46a
paige <88659700+paishee@users.noreply.github.com>
2024-02-27 13:55:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ github.com/paishee/choredec
 
 function chore( strings, ...values) {
     let args = values.map( (v, i) => `arg${i}` );
-    let cmd = strings.map( (v, i) => (i == strings.length-1) ? v: `${v}${args[i]}` );
-    return (new Function(...args, `return ${cmd.join("")}`))(...values);
+    let cmd = strings.map( (v, i) => (i == strings.length-1) ? v: `${v}${args[i]}` ).join("");
+    return (new Function(...args, `return ${cmd}`))(...values);
 }
 
 
