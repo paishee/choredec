877b1a35725818bacfa1fb8ce851cfdafcce0e48
paige <88659700+paishee@users.noreply.github.com>
2024-02-27 13:50:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,28 +7,9 @@ github.com/paishee/choredec
 
 
 function chore( strings, ...values) {
-    let [ name ] = (strings instanceof Array) ? strings : [strings];
-    let stuff = name;
-
-    function format(str) {
-        return str.replace(":", "").replace("=", "").replace(/ +(?= )/g, "").trim()
-    }
-
-    if (name == '') {
-        name = values.shift();
-        stuff = format(arguments[0][1]);
-    }
-
-    let value;
-    if (values.length == 0) value = eval(
-        format(((stuff.includes(":")) ? stuff.split(":") : stuff.split("=")).pop())
-    );
-    else value = values[0];
-
-    name = name.split(" ")[0];
-    name = format(name);
-
-    return (new Function(`value`, `return ${name} = value`))(value);
+    let args = values.map( (v, i) => `arg${i}` );
+    let cmd = strings.map( (v, i) => (i == strings.length-1) ? v: `${v}${args[i]}` );
+    return (new Function(...args, `return ${cmd.join("")}`))(...values);
 }
 
 
