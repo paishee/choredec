4c2e968693b529b7d809282a4d8557a24ae566a2
paige <88659700+paishee@users.noreply.github.com>
2024-02-27 13:10:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -19,17 +19,14 @@ function chore( strings, ...values) {
         stuff = format(arguments[0][1]);
     }
 
-
     let value;
     if (values.length == 0) value = eval(
         format(((stuff.includes(":")) ? stuff.split(":") : stuff.split("=")).pop())
     );
     else value = values[0];
 
-
     name = name.split(" ")[0];
     name = format(name);
-
 
     return (new Function(`value`, `return ${name} = value`))(value);
 }
